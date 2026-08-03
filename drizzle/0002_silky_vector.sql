318c8435b7482b2a229211b37bdf4b621eba266c
zhangjuewei20140528 <134730470+zhangjuewei20140528@users.noreply.github.com>
2026-08-03 19:44:03 +0800

    Update 0002_silky_vector.sql

--- drizzle/0002_silky_vector.sql
+++ drizzle/0002_silky_vector.sql
@@ -1,1 +1,3 @@
-
+DROP INDEX "wechat_identity_unique";--> statement-breakpoint
+CREATE UNIQUE INDEX "wechat_account_unique" ON "wechat_bindings" USING btree ("account_id");--> statement-breakpoint
+CREATE UNIQUE INDEX "wechat_user_identity_unique" ON "wechat_bindings" USING btree ("weixin_user_id");
