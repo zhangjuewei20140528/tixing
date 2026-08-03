02e2aeee2572aeea2809c307df674436dd2c3cbd
zhangjuewei20140528 <134730470+zhangjuewei20140528@users.noreply.github.com>
2026-08-03 19:44:17 +0800

    Update 0004_crazy_punisher.sql

--- drizzle/0004_crazy_punisher.sql
+++ drizzle/0004_crazy_punisher.sql
@@ -1,1 +1,4 @@
-
+ALTER TABLE "reminders" ADD COLUMN "source_channel" text;--> statement-breakpoint
+ALTER TABLE "reminders" ADD COLUMN "source_message_id" text;--> statement-breakpoint
+ALTER TABLE "wechat_bindings" ADD COLUMN "get_updates_buf" text DEFAULT '' NOT NULL;--> statement-breakpoint
+CREATE UNIQUE INDEX "reminder_source_message_unique" ON "reminders" USING btree ("source_message_id");
