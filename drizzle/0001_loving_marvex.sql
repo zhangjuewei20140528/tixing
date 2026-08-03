83551ed12f423d8c6c215a38dd7f544477231eae
zhangjuewei20140528 <134730470+zhangjuewei20140528@users.noreply.github.com>
2026-08-03 19:43:56 +0800

    Update 0001_loving_marvex.sql

--- drizzle/0001_loving_marvex.sql
+++ drizzle/0001_loving_marvex.sql
@@ -1,1 +1,2 @@
-
+ALTER TABLE "wechat_binding_sessions" ADD COLUMN "connector_base_url" text DEFAULT 'https://ilinkai.weixin.qq.com' NOT NULL;--> statement-breakpoint
+ALTER TABLE "wechat_binding_sessions" ADD COLUMN "pending_verify_code" text;
