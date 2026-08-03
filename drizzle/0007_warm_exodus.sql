d5da863742c057a86d3358033369e99bb6aefc53
zhangjuewei20140528 <134730470+zhangjuewei20140528@users.noreply.github.com>
2026-08-03 19:44:37 +0800

    Update 0007_warm_exodus.sql

--- drizzle/0007_warm_exodus.sql
+++ drizzle/0007_warm_exodus.sql
@@ -1,1 +1,3 @@
-
+ALTER TABLE "users" ADD COLUMN "account_status" text DEFAULT 'active' NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "vip_type" text DEFAULT 'none' NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "vip_expires_at" timestamp with time zone;
