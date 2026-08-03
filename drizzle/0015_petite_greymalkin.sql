ab1c4647a4ca0f5af34cf5904c08cb43cb1ed9da
zhangjuewei20140528 <134730470+zhangjuewei20140528@users.noreply.github.com>
2026-08-03 19:45:37 +0800

    Update 0015_petite_greymalkin.sql

--- drizzle/0015_petite_greymalkin.sql
+++ drizzle/0015_petite_greymalkin.sql
@@ -1,1 +1,4 @@
-
+ALTER TABLE "delivery_attempts" ADD COLUMN "handled_at" timestamp with time zone;--> statement-breakpoint
+ALTER TABLE "delivery_attempts" ADD COLUMN "handled_by" uuid;--> statement-breakpoint
+ALTER TABLE "delivery_attempts" ADD COLUMN "handling_note" text;--> statement-breakpoint
+ALTER TABLE "delivery_attempts" ADD CONSTRAINT "delivery_attempts_handled_by_users_id_fk" FOREIGN KEY ("handled_by") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;
