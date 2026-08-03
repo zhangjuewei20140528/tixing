ec57e773dfd9262b26d9c2bd8d571669b9c0619d
zhangjuewei20140528 <134730470+zhangjuewei20140528@users.noreply.github.com>
2026-08-03 19:44:30 +0800

    Update 0006_acoustic_jack_murdock.sql

--- drizzle/0006_acoustic_jack_murdock.sql
+++ drizzle/0006_acoustic_jack_murdock.sql
@@ -1,1 +1,5 @@
-
+ALTER TABLE "users" ALTER COLUMN "phone" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "username" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "password_hash" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "role" text DEFAULT 'user' NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_username_unique" UNIQUE("username");
